06b3550b3a08775eaff572ea882e588312521c60
Jake Armstrong <tefman7422@gmail.com>
2018-07-31 15:20:10 -0400

    Working Update (For School)
    Just a working update for while I am at school. The previous
    "Functionality," which was only fluff, has now been removed. I want this
    bot tto work.

---
+++ cogs/util/settings.py
@@ -0,0 +1,6 @@
+
+DEFAULT_PATH = "data/Lum/settings.json"
+
+class Settings:
+    def __init__(self):
+        self.login_cred = None
--- lamu.py
+++ lamu.py
@@ -1,27 +1,66 @@
+import asyncio
+from cogs.util.settings import Settings
 try:
     import discord
     from discord.ext import commands
 except ImportError:
-    print("Whoops! Discord.py not installed! -I")
-    input("...")
+    print("Error Importing Discord, Try Reinstalling")
+    input("Press 'Enter' to continue... ")
     exit(1)
 
+BOT_VERSION = "0.0.0a" #Remember to Change this
+
 class Lum(commands.Bot):
-
     def __init__(self, *args, **kwargs):
-	
-        super.__init__(*args, **kwargs)
-		
-		#Quick mental note the prefix thingy does not prevent the on message event!
+        self.settings = Settings()
+        self.version = BOT_VERSION
+        self.intro_played = False
 
-def main(bot):
-	
-	yield from bot.login(token)
-		
+def playIntro():
+    import cogs.util.intro as intro
+    intro.play(BOT_VERSION)
+    
+def setup(Settings):
+    first_run = Settings.bot_settings == Settings.default_settings
+    if first_run:
+        #Todo add a token info adder fucker.
+        pass
+
+def main(Lum):
+    
+    setup(Lum.settings)
+    
+    if Lum.settings.login_cred:
+        yield from Lum.login(*Lum.settings.login_cred)
+    #yield from Lum.connect()  --Support for Voice Channels
+
+def initialize(bot=Lum):
+    lum = bot()
+    #insert bot declarations here
+
+    @lum.event
+    async def on_ready():
+        if not lum.intro_played:
+            playIntro()
+            lum.intro_played = True
+
+    return lum
+
 if __name__ == "__main__":
-    print("Test")
-    input("Stay On Screen")
-	lamu = Lum()
-	
-	
-    exit(0)
+    loop = asyncio.get_event_loop()
+    #Initialize lum here
+    Lamu = initialize()
+    try:
+        loop.run_until_complete(main(Lamu))
+    except KeyboardInterrupt:
+        #Logout of discord
+        loop.run_until_complete(Lamu.logout())
+
+    except Exception:
+        #Logout of Discord
+        pass
+        loop.run_until_complete(Lamu.logout())
+
+    finally:
+        loop.close()
+        exit(0)
