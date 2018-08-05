9930a1a034e916061d6428a9cef65fd3f5938aea
Jake Armstrong <tefman7422@gmail.com>
2018-08-04 23:16:30 -0400

    1 File didn't sync with the rest of it....
    OOF

--- cogs/util/settings.py
+++ cogs/util/settings.py
@@ -5,11 +5,17 @@ class Settings:
     def __init__(self):
         self.bot_settings = joho.load(joho, DEFAULT_PATH)
         self.default_settings = {
+            "version":"0.1.0a",
             "TOKEN":"",
-            "First-Run":"True"
+            "First-Run":"True",
+            "OWNER":""
         }
-        self.prefixes = "ラム"
-
+        self.prefixes = ";"
+        self.token = self.bot_settings['TOKEN']
+        self.restart = False
+        self.owner = self.bot_settings['OWNER']
+        self.version = self.bot_settings['VERSION']
+        
     def save(self):
 
         joho.save(joho, self.bot_settings, DEFAULT_PATH)
