40fd88efa51a39bd1f1df762fd746f6da1d5d3e5
Bak3dChips <tefman7422@gmail.com>
2018-08-28 15:04:51 -0400

    School Working Update

--- lamu.py
+++ lamu.py
@@ -6,33 +6,53 @@ from discord.ext import commands
 
 from util.settings import Settings
 
+#REMEMBER! THIS BOT IS ONLY FOR SMALL SERVER NETWORKS, NOT LARGE SCALE INVITING.
 class LumBot(commands.Bot):
     def __init__(self):
-        self.settings = Settings(LumBot.__class__)
+		self._default_settings = {"token":None}
+        self._settings = Settings(LumBot.__class__)
         self.restart = False
+		self._data
+		
+		def get_prefix(bot, message):
+			pass
+			#fill void with something that retrieves prefix for server, incase they use their bot account for multiple servers.
+		
         super().__init__(get_prefix, pm_help=True)
 
     async def on_message(self, message):
         await self.process_commands(message)
 
     async def on_ready(self):
-        print("Ready-だちゃ")
+        print("Ready-だちゃ!")
 
+	def getSetting(set_name):
+		return self._settings.load(set_name)
+		
+	def setSetting(set_name, data):
+		self._settings.set(set_name, data)
+		return data
 
 def load_cogs(bot):
     bot.load_extension("core.owner")
 
 async def start(bot):
-    try:
-        await bot.login(bot.settings.token)
-    except Exception:
-        #Add A smarter thing that tells you that you didn't provide a good token
-		pass
-    await bot.connect()
+	try:
+		await bot.login(bot.getSetting(["token"]))
+	except discord.LoginFailure:
+		print("Oops! I think gave me an invalid token!")
+		print("Would you like me to reset it?")
+		if input("> ").strip().lower() == "y":
+			bot.setSetting("token", None)
+		
+	await bot.connect()
+		
 
 def init():
     bot = LumBot()
     load_cogs(bot)
+	
+	if 
 
     @bot.command()
     async def ping(ctx):
@@ -40,17 +60,19 @@ def init():
         await ctx.message.channel.send("Pong!")
 
     return bot
-
+	
+	
 if __name__ == "__main__":
     #logging.basicConfig(level=logging.INFO)
     bot = init()
-    
+	
     loop = asyncio.get_event_loop()
 
     try:
         loop.run_until_complete(start(bot))
     except KeyboardInterrupt:
         loop.run_until_complete(bot.logout())
+		exit(1)
     except Exception:
         exit(1)
     finally:
@@ -58,3 +80,4 @@ if __name__ == "__main__":
             exit(78)
         else:
             exit(0)
+			
--- util/settings.py
+++ util/settings.py
@@ -10,20 +10,25 @@ class Settings:
 		try:
 			self._data = self._load()
 		except OSError:
-			self._data = object.default_settings 
+			self._data = object._default_settings 
 			with open(file_location, "w") as f:
 				f.write(self._data)
-		
 	
-	def load(self, guild, data):
+	def load(self, data):
 		data = self._load(self._file_location)
-		return[guild][data]
+		return[data]
 	
-	
-
+	def set(self,set_name, data):
+		self._data[set_name] = data
+		self.save()
+		
     def _load(self,file_location):
-        """Returns 'ded' at the moment"""
+        """Loads the JSON file for use"""
         if file_location.endswith(".json"):
             with open(file_location) as f:
                 return json.load(f)
 		
+		
+	def save(self):
+		with open(self._file_location) as f:
+			f.write(json.dumps(self._data))
