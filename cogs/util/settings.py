2a52b236aaa4d882975a0683979e2b9b77860d75
Bak3dChips <tefman7422@gmail.com>
2018-08-31 13:43:02 -0400

    SETTINGS works UTF-8, Auto-Appending

--- cogs/util/settings.py
+++ cogs/util/settings.py
@@ -8,34 +8,61 @@ class Settings:
 		self._file_location  = "data/settings/"+ supplanter
 		self._data = self._load(def_settings , supplanter)
 		self.default = False
+		
 		def old_detector(def_settings):
+			#print("Detecting Old Settings")
 			newSettings = False
-			for k in list(self._data):
+			
+			for k in self._data:
+				#print(k)
 				if k not in def_settings:
+					#print("Old Settings found")
 					newSettings = True
+			
+			#print("Checking new Settings")
 			if newSettings:
-				with open(self._file_location+"_old_","w+") as f:
-					json.dump(def_settings,f)
+			
+				with open(self._file_location+"_old_","w",encoding="utf8") as f:
+					json.dump(self._data,f,ensure_ascii=False)
+					#print("Dumping old Data")
+					
+				with open(self._file_location+"","w",encoding="utf8") as f:
+					json.dump(def_settings,f,ensure_ascii=False)
+					#print("Loading New Data")
+					
 				temp = self._data
 				self._data = def_settings
 				for k in list(temp):
 					if k in self._data:
 						self._data[k] = temp[k]
+						#print("Loading reloadable data")
+						self.save()
+						return
+						
+			#SECTION TWO -- Append any non-existant Setting
+			for i in def_settings:
+				if i not in self._data:
+					self._data[i] = def_settings[i]
+					self.save()
+				
+						
+			
 				
 		old_detector(def_settings)
 
 	def _load(self,def_settings,supplanter):
 		"""Reads file data """
-		#Needs to be made better
+		#print("Checking DIRS")
 		os.makedirs(self._file_location.replace(supplanter,""), exist_ok=True)
 		try:
 			#Tries to load the file if the location exists
-			with open(self._file_location) as f:
+			with open(self._file_location, encoding="utf8") as f:
+				#print("loading data")
 				return json.load(f)
-		except (FileNotFoundError, json.decoder.JSONDecodeError):
-			#Creates a default file location
-			with open(self._file_location, "w+") as f:
-				json.dump(def_settings, f)
+		except (FileNotFoundError, json.decoder.JSONDecodeError) as e:
+			with open(self._file_location, "w+",encoding="utf8") as f:
+				#print("Writing default Data")
+				json.dump(def_settings, f, ensure_ascii=False)
 				return def_settings
 
 	def get(self,key):
@@ -46,6 +73,6 @@ class Settings:
 		return data
 				
 	def save(self):
-		with open(self._file_location, "w+") as f:
-			json.dump(self._data,f)
+		with open(self._file_location, "w+", encoding="utf8") as f:
+			json.dump(self._data,f,ensure_ascii=False)
 
